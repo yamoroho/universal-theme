f64a20ec1e2afbc36a85242764b69bd4681235e7
yamoroho <ymorokhovets@gmail.com>
2021-03-09 09:50:51 +0200

    Исправление ошибок

--- functions.php
+++ functions.php
@@ -31,9 +31,20 @@ add_action( 'after_setup_theme', 'universal_theme_setup');
 function universal_theme_widgets_init() {
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar in front-page', 'universal-theme' ),
+			'name'          => esc_html__( 'Sidebar in front-page', 'universaltheme' ),
 			'id'            => 'main-sidebar',
-			'description'   => esc_html__( 'Добавьте виджеты сюда.', 'universal-theme' ),
+			'description'   => esc_html__( 'Добавьте виджеты сюда.', 'universaltheme' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+  register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar from posts', 'universaltheme' ),
+			'id'            => 'posts-sidebar',
+			'description'   => esc_html__( 'Добавьте посты сюда.', 'universaltheme' ),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
